8011d5e5ab3c980a33807a94c2239042bd0d8ad2
soundbylaura <158212598+soundbylaura@users.noreply.github.com>
2024-06-02 17:26:43 +0000

    Completed Part 1c-Step 4 - Added 10th UDT
    Added 10th UDT Audio Interface and its sub-objects and actionables

--- main.cpp
+++ main.cpp
@@ -337,17 +337,17 @@ Define an object that is made of 5 sub-objects.
     These 5 sub-objects will not be defined using Primitives, but instead will be their own UDTs 
     you'll define these 5 sub-objects in Part 1d.
 
-Thing 10)
+Thing 10) Audio Interface
 5 properties:
-    1)
-    2)
-    3)
-    4)
-    5)
+    1) number of inputs
+    2) types of inputs
+    3) types of enclosure material
+    4) types of outputs
+    5) locations of inputs
 3 things it can do:
-    1)
-    2)
-    3)
+    1) receive audio signals
+    2) display signal levels
+    3) output audio and MIDI signals
  */
 
  /*
