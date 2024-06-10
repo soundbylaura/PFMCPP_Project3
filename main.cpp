ab119d77f8e9d8c992ce6d8995293c8560e5fc6e
matkatmusic <matkatmusic@gmail.com>
2024-06-10 01:23:59 +0000

    Fixed incorrect indentations

--- main.cpp
+++ main.cpp
@@ -877,330 +877,330 @@ Fix anything that needs fixing
 
 You should see "good to go" in the program output
 */
-    struct Bicycle
+struct Bicycle
+{
+
+    //number of gears (int)
+    int numGears = 18;
+    //amount of air required in tires in PSI (float)
+    float tireAirPressure = 70.0f;
+    //type of handlebars (std::string)
+    std::string handlebar = ("Drop" "Flat");
+    //number of water bottle holders (int)
+    int bottleHolders = 3;
+    //number of times used per week (int)
+    int rides = 5;
+
+    //3 things it can do:
+    // transport a person
+    void transportPerson();
+
+    // roll down a hill
+    void rollDownhill();
+
+    // need repairs
+    void repairs();
+};
+/*
+
+Thing 2) Recording Studio
+
+*/
+struct RecordingStudio
+{
+
+    //number of microphones (int)
+    int microphones = 28;
+    //number of clients (int)
+    int numOfClients = 33;
+    //cost of food needed per week (float)
+    float expenseCatering = 300.0f;
+    //cost of business insurance (float)
+    float expenseInsurance = 1100.0f;
+    //number of employees (int)
+    int employees = 8;
+
+    struct ControlRoom
     {
+        bool isBooked = false;
+        float ratePerHour = 500.0f;
+        std::string consoleType = "Analog";
+        bool clientHasEngineer = false;
+        int hoursBooked = 8;
 
-        //number of gears (int)
-        int numGears = 18;
-        //amount of air required in tires in PSI (float)
-        float tireAirPressure = 70.0f;
-        //type of handlebars (std::string)
-        std::string handlebar = ("Drop" "Flat");
-        //number of water bottle holders (int)
-        int bottleHolders = 3;
-        //number of times used per week (int)
-        int rides = 5;
-
-        //3 things it can do:
-        // transport a person
-        void transportPerson();
-
-        // roll down a hill
-        void rollDownhill();
-
-        // need repairs
-        void repairs();
+        void bookRoom (bool isBooked = true, int numberOfClients = 5);
+        void prepareRoom (bool powerOn = true, bool roomCleaned = true, int numberOfAssistants = 1, float rateForAssistant = 5.0f);
+        int caclulateTotalFee (int numberOfActualHours = 12, float overtimeRate = 650.50f, int tapesUsed = 4);
     };
-    /*
 
-    Thing 2) Recording Studio
+    void beginRecordingSession (ControlRoom controlRoomA);
 
-    */
-    struct RecordingStudio
+    void startRateClock (bool hasGroupon = true);
+
+    void closeRoom (ControlRoom controlRoomA, bool equipmentOff = true);
+
+    int billClient (ControlRoom controlRoomA, int hoursUsed = 12, int tapesUsed = 3, int woofersBlown = 3);
+
+    //3 things it can do:
+    // record audio
+    void recordAudio();
+
+    // send invoices to clients
+    void sendInvoice();
+
+    // host events
+    void hostEvent();
+};
+/*
+
+Thing 3) Audio Interface
+
+*/
+struct AudioInterface
+{
+
+    //number of inputs (int)
+    int audioInputs = 16;
+    //types of inputs (std::string)
+    std::string inputType = ("XLR" "TRS" "MIDI" "Optical");
+    //types of enclosure material (std::string)
+    std::string enclosureMaterial = ("Metal" "Plastic");
+    //types of outputs (std::string)
+    std::string outputType = ("XLR" "TRS" "MIDI" "Optical" "RCA");
+    //locations of inputs (std::string)
+    std::string inputsLocation = ("Front" "Back");
+
+    //3 things it can do:
+    // receive audio signals
+    void receiveAudio();
+
+    // display signal levels
+    float displayLevels(); //return is displayed for user
+
+    // output audio signals
+    void outputAudio();
+};
+/*
+
+Thing 4) Stereo Widener Audio Plugin
+
+*/
+struct StereoWidenerAudioPlugin
+{
+
+    //number of GUI elements (int)
+    int GUIElements = 2;
+    //color of interface elements (std::string)
+    std::string knobsColors = ("Black" "Gray" "Red");
+    //amount of memory required (float)
+    float memoryAllocated = 64.0f;
+    //font size of tool tips (int)
+    int fontSizeToolTips = 32;
+    //location of interface elements within GUI (float)
+    float knobsLocation = 0;
+
+    struct MixKnob
     {
+        std::string label = "Mix";
+        int sliderHeight = 10; 
+        int sliderWidth = 2;
+        int ticksOnSlider = 50;
+        std::string sliderColor = "Black";
 
-        //number of microphones (int)
-        int microphones = 28;
-        //number of clients (int)
-        int numOfClients = 33;
-        //cost of food needed per week (float)
-        float expenseCatering = 300.0f;
-        //cost of business insurance (float)
-        float expenseInsurance = 1100.0f;
-        //number of employees (int)
-        int employees = 8;
+        void getStateInformation (int sizeInBytes, float inputLevel);
+        void prepareToPlay (double sampleRate, int samplesPerBlock);
+        bool isBypassed (bool customBypassButton, bool nativeBypassButton);
 
-        struct ControlRoom
-        {
-            bool isBooked = false;
-            float ratePerHour = 500.0f;
-            std::string consoleType = "Analog";
-            bool clientHasEngineer = false;
-            int hoursBooked = 8;
-
-            void bookRoom (bool isBooked = true, int numberOfClients = 5);
-            void prepareRoom (bool powerOn = true, bool roomCleaned = true, int numberOfAssistants = 1, float rateForAssistant = 5.0f);
-            int caclulateTotalFee (int numberOfActualHours = 12, float overtimeRate = 650.50f, int tapesUsed = 4);
-        };
-
-        void beginRecordingSession (ControlRoom controlRoomA);
-
-        void startRateClock (bool hasGroupon = true);
-
-        void closeRoom (ControlRoom controlRoomA, bool equipmentOff = true);
-
-        int billClient (ControlRoom controlRoomA, int hoursUsed = 12, int tapesUsed = 3, int woofersBlown = 3);
-
-        //3 things it can do:
-        // record audio
-        void recordAudio();
-
-        // send invoices to clients
-        void sendInvoice();
-
-        // host events
-        void hostEvent();
     };
-    /*
 
-    Thing 3) Audio Interface
+    void increaseWetness (MixKnob increase);
+    void decreaseWetness (MixKnob decrease);
+    void smartMute (int audioInput = 1, float audioOutput = 1.1f);
 
-    */
-    struct AudioInterface
-    {
+    //3 things it can do:
+    // capture audio signal
+    void captureAudio();
 
-        //number of inputs (int)
-        int audioInputs = 16;
-        //types of inputs (std::string)
-        std::string inputType = ("XLR" "TRS" "MIDI" "Optical");
-        //types of enclosure material (std::string)
-        std::string enclosureMaterial = ("Metal" "Plastic");
-        //types of outputs (std::string)
-        std::string outputType = ("XLR" "TRS" "MIDI" "Optical" "RCA");
-        //locations of inputs (std::string)
-        std::string inputsLocation = ("Front" "Back");
+    // display text descriptions
+    char textInfo(); //return is displayed for user
 
-        //3 things it can do:
-        // receive audio signals
-        void receiveAudio();
+    // widen audio signal
+    float widenSignal(); //return is related to function parameter
+};
+/*
 
-        // display signal levels
-        float displayLevels(); //return is displayed for user
+Thing 5) Graphical User Interface
 
-        // output audio signals
-        void outputAudio();
-    };
-    /*
+*/
+struct GraphicalUserInterface
+{
 
-    Thing 4) Stereo Widener Audio Plugin
+    //width in cm (int)
+    int GUIWidth = 10;
+    //height in cm (int)
+    int GUIHeight = 10;
+    //number of frequency selector dials (int)
+    int freqDialsQuantity = 3;
+    //names of frequency selector dials (std::string)
+    std::string freqDialsNames = ("Freq" "Gain" "Q");
+    //color of background (std::string)
+    std::string backgroundColor = "Purple";
 
-    */
-    struct StereoWidenerAudioPlugin
-    {
+    //3 things it can do:
+    // display input level
+    float displayInputLevel(); //return is displayed for user
 
-        //number of GUI elements (int)
-        int GUIElements = 2;
-        //color of interface elements (std::string)
-        std::string knobsColors = ("Black" "Gray" "Red");
-        //amount of memory required (float)
-        float memoryAllocated = 64.0f;
-        //font size of tool tips (int)
-        int fontSizeToolTips = 32;
-        //location of interface elements within GUI (float)
-        float knobsLocation = 0;
+    // display attenuation level(s) of frequency band(s)
+    float displayAttenuation(); //return is displayed for user
 
-        struct MixKnob
-        {
-            std::string label = "Mix";
-            int sliderHeight = 10; 
-            int sliderWidth = 2;
-            int ticksOnSlider = 50;
-            std::string sliderColor = "Black";
+    // allow user to modify parameters
+    float parameterMod(); //return is related to function parameter
+};
+/*
 
-            void getStateInformation (int sizeInBytes, float inputLevel);
-            void prepareToPlay (double sampleRate, int samplesPerBlock);
-            bool isBypassed (bool customBypassButton, bool nativeBypassButton);
+Thing 6) License
 
-        };
+*/
+struct License
+{
 
-        void increaseWetness (MixKnob increase);
-        void decreaseWetness (MixKnob decrease);
-        void smartMute (int audioInput = 1, float audioOutput = 1.1f);
+    //text (std::string)
+    std::string bodyText = "You must be this tall to ride this ride.";
+    //font size (int)
+    int bodyTextFontSize = 18;
+    //format (std::string)
+    std::string fileFormat = "PDF";
+    //text color (std::string)
+    std::string bodyTextColor = "black";
+    //executed (bool)
+    bool isExecuted = true;
 
-        //3 things it can do:
-        // capture audio signal
-        void captureAudio();
+    //3 things it can do:
+    // provide information
+    char displayTextBody(); //return is displayed for user
 
-        // display text descriptions
-        char textInfo(); //return is displayed for user
+    // link to manufacturer website
+    char website(); //return is actionable by user
 
-        // widen audio signal
-        float widenSignal(); //return is related to function parameter
-    };
-    /*
+    // not be copied
+    void copyProtection();
+};
+/*
 
-    Thing 5) Graphical User Interface
+Thing 7) Company
 
-    */
-    struct GraphicalUserInterface
-    {
+*/
+struct Company
+{
 
-        //width in cm (int)
-        int GUIWidth = 10;
-        //height in cm (int)
-        int GUIHeight = 10;
-        //number of frequency selector dials (int)
-        int freqDialsQuantity = 3;
-        //names of frequency selector dials (std::string)
-        std::string freqDialsNames = ("Freq" "Gain" "Q");
-        //color of background (std::string)
-        std::string backgroundColor = "Purple";
+    //name (std::string)
+    std::string companyName = "SBL Plugins";
+    //location (std::string)
+    std::string companyLocation = "Atlanta";
+    //number of employees (int)
+    int companyEmployees = 1;
+    //amount of income per year (float)
+    float companyIncome = 0.0f;
+    //cost of office equipment (float)
+    float equipmentCost = 200.25f;
 
-        //3 things it can do:
-        // display input level
-        float displayInputLevel(); //return is displayed for user
+    //3 things it can do:
+    // create plugins
+    int createPlugin(); //return is related to function parameter
 
-        // display attenuation level(s) of frequency band(s)
-        float displayAttenuation(); //return is displayed for user
+    // set authorization methods
+    void authEnable();
 
-        // allow user to modify parameters
-        float parameterMod(); //return is related to function parameter
-    };
-    /*
+    // sign legal agreements
+    void signContract();
+};
+/*
 
-    Thing 6) License
+Thing 8) Signal Processor
 
-    */
-    struct License
-    {
+*/
+struct SignalProcessor
+{
 
-        //text (std::string)
-        std::string bodyText = "You must be this tall to ride this ride.";
-        //font size (int)
-        int bodyTextFontSize = 18;
-        //format (std::string)
-        std::string fileFormat = "PDF";
-        //text color (std::string)
-        std::string bodyTextColor = "black";
-        //executed (bool)
-        bool isExecuted = true;
+    //samples (float)
+    float numSamples = 256.0f;
+    //channels (int)
+    int numChannels = 2;
+    //type (std::string)
+    std::string type = "Unknown";
+    //buffer (float)
+    float numBuffer = 2.2f;
+    //program (int)
+    int program = 4;
 
-        //3 things it can do:
-        // provide information
-        char displayTextBody(); //return is displayed for user
+    //3 things it can do:
+    // receive audio
+    void receiveAudio();
 
-        // link to manufacturer website
-        char website(); //return is actionable by user
+    // convert raw audio level to decibels
+    float convertToDecibels(); //return is related to function parameter
 
-        // not be copied
-        void copyProtection();
-    };
-    /*
+    // send audio
+    void sendAudio();
+};
+/*
 
-    Thing 7) Company
+Thing 9) DSP Engine
 
-    */
-    struct Company
-    {
+*/
+struct DSPEngine
+{
 
-        //name (std::string)
-        std::string companyName = "SBL Plugins";
-        //location (std::string)
-        std::string companyLocation = "Atlanta";
-        //number of employees (int)
-        int companyEmployees = 1;
-        //amount of income per year (float)
-        float companyIncome = 0.0f;
-        //cost of office equipment (float)
-        float equipmentCost = 200.25f;
+    //value of incoming gain (double)
+    double gainInputLevel = 0;
+    //duration of gain ramp (double)
+    double gainRampDuration = 3482;
+    //amount of effect (float)
+    float wetLevel = 33.1f;
+    //name of effect (std::string)
+    std::string effect = ("Chorus" "Phase" "Flange");
+    //value of output level (float)
+    float gainOutputLevel = -6.4f;
 
-        //3 things it can do:
-        // create plugins
-        int createPlugin(); //return is related to function parameter
+    // 3 things it can do:
+    // create a chorus effect
+    void createChorus();
 
-        // set authorization methods
-        void authEnable();
+    // modify gain level
+    float modifyGain(); //return is related to function parameter
 
-        // sign legal agreements
-        void signContract();
-    };
-    /*
+    // disable output
+    bool outputEnabled(); //return is related to function parameter
+};
+/*
 
-    Thing 8) Signal Processor
+Thing 10) Equalizer Audio Plugin
 
-    */
-    struct SignalProcessor
-    {
+*/
+struct EqualizerAudioPlugin
+{
 
-        //samples (float)
-        float numSamples = 256.0f;
-        //channels (int)
-        int numChannels = 2;
-        //type (std::string)
-        std::string type = "Unknown";
-        //buffer (float)
-        float numBuffer = 2.2f;
-        //program (int)
-        int program = 4;
+    //Graphical User Interface
+    GraphicalUserInterface GUI;
+    //license
+    License license;
+    //company
+    Company company;
+    //signal processor
+    SignalProcessor signalProcessor;
+    //DSP engine
+    DSPEngine DSP_Engine;
 
-        //3 things it can do:
-        // receive audio
-        void receiveAudio();
+    // 3 things it can do:
+    // display changes to frequency level
+    float displayFreqLevelChange(); //return is displayed for user
 
-        // convert raw audio level to decibels
-        float convertToDecibels(); //return is related to function parameter
+    // attenuate audio frequency level
+    float freqLevelChange(); //return is related to function parameter
 
-        // send audio
-        void sendAudio();
-    };
-    /*
-
-    Thing 9) DSP Engine
-
-    */
-    struct DSPEngine
-    {
-
-        //value of incoming gain (double)
-        double gainInputLevel = 0;
-        //duration of gain ramp (double)
-        double gainRampDuration = 3482;
-        //amount of effect (float)
-        float wetLevel = 33.1f;
-        //name of effect (std::string)
-        std::string effect = ("Chorus" "Phase" "Flange");
-        //value of output level (float)
-        float gainOutputLevel = -6.4f;
-
-        // 3 things it can do:
-        // create a chorus effect
-        void createChorus();
-
-        // modify gain level
-        float modifyGain(); //return is related to function parameter
-
-        // disable output
-        bool outputEnabled(); //return is related to function parameter
-    };
-    /*
-
-    Thing 10) Equalizer Audio Plugin
-
-    */
-    struct EqualizerAudioPlugin
-    {
-
-        //Graphical User Interface
-        GraphicalUserInterface GUI;
-        //license
-        License license;
-        //company
-        Company company;
-        //signal processor
-        SignalProcessor signalProcessor;
-        //DSP engine
-        DSPEngine DSP_Engine;
-
-        // 3 things it can do:
-        // display changes to frequency level
-        float displayFreqLevelChange(); //return is displayed for user
-
-        // attenuate audio frequency level
-        float freqLevelChange(); //return is related to function parameter
-
-        // accomodate multiple hosts
-        void buildType();
-    };
+    // accomodate multiple hosts
+    void buildType();
+};
 
   
 /*
