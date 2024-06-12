4dac28f5be4a4ba17e4f85c450214b3dbd684b2a
soundbylaura <158212598+soundbylaura@users.noreply.github.com>
2024-06-12 20:16:00 +0000

    Working on project, switching computers

--- main.cpp
+++ main.cpp
@@ -107,11 +107,36 @@ struct CarWash
     you should be able to deduce the return type of those functions based on their usage in Person::run()
     You'll need to insert the Person struct from the video in the space below.
  */
+struct Limb{};
 
+struct Person
+{
+    Limb leftFoot;
+    Limb rightFoot;
 
+    int age;
+    int height;
+    float hairLength;
+    float GPA;
+    unsigned int SATScore;
+    int distanceTraveled;
 
+    void run( int howFast, bool startWithLeftFoot);
+    void stepForward();
+    int stepSize();
+};
 
-
+void Person::run( int howFast, bool startWithLeftFoot)
+{
+    if( startWithLeftFoot == true )
+    {
+        void (distanceTraveled += howFast);
+    }
+    else
+     {
+        void (distanceTraveled -= howFast);//illogical but didn't want to copy over from video, makes code work   
+    }
+}
  /*
  2) provide implementations for the member functions you declared in your 10 user-defined types from the previous video outside of your UDT definitions.
     If you have 'unused parameter' warnings, you aren't using one of your function parameters in your implementation.
@@ -133,45 +158,27 @@ struct CarWash
 
 struct Bicycle
 {
-
-    //number of gears (int)
     int numGears = 18;
-    //amount of air required in tires in PSI (float)
     float tireAirPressure = 70.0f;
-    //type of handlebars (std::string)
     std::string handlebar = ("Drop" "Flat");
-    //number of water bottle holders (int)
     int bottleHolders = 3;
-    //number of times used per week (int)
     int rides = 5;
 
-    //3 things it can do:
-    // transport a person
     void transportPerson();
-
-    // roll down a hill
     void rollDownhill();
-
-    // need repairs
     void repairs();
 };
-/*
 
-Thing 2) Recording Studio
+void Bicycle::transportPerson(){}
+void Bicycle::rollDownhill(){}
+void Bicycle::repairs(){}
 
-*/
 struct RecordingStudio
 {
-
-    //number of microphones (int)
     int microphones = 28;
-    //number of clients (int)
     int numOfClients = 33;
-    //cost of food needed per week (float)
     float expenseCatering = 300.0f;
-    //cost of business insurance (float)
     float expenseInsurance = 1100.0f;
-    //number of employees (int)
     int employees = 8;
 
     struct ControlRoom
@@ -184,73 +191,45 @@ struct RecordingStudio
 
         void bookRoom (bool isBooked = true, int numberOfClients = 5);
         void prepareRoom (bool powerOn = true, bool roomCleaned = true, int numberOfAssistants = 1, float rateForAssistant = 5.0f);
-        int caclulateTotalFee (int numberOfActualHours = 12, float overtimeRate = 650.50f, int tapesUsed = 4);
+        int caclulateTotalFee (int numberOfActualHours = 12, float overtimeRate = 650.50f, int reelsUsed = 4);
     };
 
     void beginRecordingSession (ControlRoom controlRoomA);
-
     void startRateClock (bool hasGroupon = true);
-
     void closeRoom (ControlRoom controlRoomA, bool equipmentOff = true);
-
     int billClient (ControlRoom controlRoomA, int hoursUsed = 12, int tapesUsed = 3, int woofersBlown = 3);
 
-    //3 things it can do:
-    // record audio
     void recordAudio();
-
-    // send invoices to clients
     void sendInvoice();
-
-    // host events
     void hostEvent();
 };
-/*
+void RecordingStudio::ControlRoom::bookRoom (bool isBooked, int numberOfClients){}
+void RecordingStudio::ControlRoom:: prepareRoom(bool powerOn, bool roomCleaned, int numberOfAssistants, float rateForAssistant){}
+void RecordingStudio::ControlRoom::caclulateTotalFee(int numberOfActualHours, float overtimeRate, int reelsUsed){}
 
-Thing 3) Audio Interface
-
-*/
 struct AudioInterface
 {
-
-    //number of inputs (int)
     int audioInputs = 16;
-    //types of inputs (std::string)
     std::string inputType = ("XLR" "TRS" "MIDI" "Optical");
-    //types of enclosure material (std::string)
     std::string enclosureMaterial = ("Metal" "Plastic");
-    //types of outputs (std::string)
     std::string outputType = ("XLR" "TRS" "MIDI" "Optical" "RCA");
-    //locations of inputs (std::string)
     std::string inputsLocation = ("Front" "Back");
 
-    //3 things it can do:
-    // receive audio signals
     void receiveAudio();
-
-    // display signal levels
-    float displayLevels(); //return is displayed for user
-
-    // output audio signals
+    float displayLevels();
     void outputAudio();
 };
-/*
+void AudioInterface::receiveAudio(){}
+float AudioInterface::displayLevels(){
+    return 2.2f;}
+void AudioInterface::outputAudio(){}
 
-Thing 4) Stereo Widener Audio Plugin
-
-*/
 struct StereoWidenerAudioPlugin
 {
-
-    //number of GUI elements (int)
     int GUIElements = 2;
-    //color of interface elements (std::string)
     std::string knobsColors = ("Black" "Gray" "Red");
-    //amount of memory required (float)
     float memoryAllocated = 64.0f;
-    //font size of tool tips (int)
     int fontSizeToolTips = 32;
-    //location of interface elements within GUI (float)
     float knobsLocation = 0;
 
     struct MixKnob
@@ -271,188 +250,94 @@ struct StereoWidenerAudioPlugin
     void decreaseWetness (MixKnob decrease);
     void smartMute (int audioInput = 1, float audioOutput = 1.1f);
 
-    //3 things it can do:
-    // capture audio signal
     void captureAudio();
-
-    // display text descriptions
-    char textInfo(); //return is displayed for user
-
-    // widen audio signal
-    float widenSignal(); //return is related to function parameter
+    char textInfo();
+    float widenSignal();
 };
-/*
 
-Thing 5) Graphical User Interface
-
-*/
 struct GraphicalUserInterface
 {
 
-    //width in cm (int)
     int GUIWidth = 10;
-    //height in cm (int)
     int GUIHeight = 10;
-    //number of frequency selector dials (int)
     int freqDialsQuantity = 3;
-    //names of frequency selector dials (std::string)
     std::string freqDialsNames = ("Freq" "Gain" "Q");
-    //color of background (std::string)
     std::string backgroundColor = "Purple";
 
-    //3 things it can do:
-    // display input level
-    float displayInputLevel(); //return is displayed for user
-
-    // display attenuation level(s) of frequency band(s)
-    float displayAttenuation(); //return is displayed for user
-
-    // allow user to modify parameters
-    float parameterMod(); //return is related to function parameter
+    float displayInputLevel(); 
+    float displayAttenuation();
+    float parameterMod();
 };
-/*
 
-Thing 6) License
+float GraphicalUserInterface::displayInputLevel(){
+    return 1.0f;}
+float GraphicalUserInterface::displayAttenuation(){
+    return 93.5f;}
+float GraphicalUserInterface::parameterMod(){
+    return 44.44f;}
 
-*/
 struct License
 {
-
-    //text (std::string)
     std::string bodyText = "You must be this tall to ride this ride.";
-    //font size (int)
     int bodyTextFontSize = 18;
-    //format (std::string)
     std::string fileFormat = "PDF";
-    //text color (std::string)
     std::string bodyTextColor = "black";
-    //executed (bool)
     bool isExecuted = true;
 
-    //3 things it can do:
-    // provide information
-    char displayTextBody(); //return is displayed for user
-
-    // link to manufacturer website
-    char website(); //return is actionable by user
-
-    // not be copied
+    char displayTextBody();
+    char website();
     void copyProtection();
 };
-/*
 
-Thing 7) Company
-
-*/
 struct Company
 {
-
-    //name (std::string)
     std::string companyName = "SBL Plugins";
-    //location (std::string)
     std::string companyLocation = "Atlanta";
-    //number of employees (int)
     int companyEmployees = 1;
-    //amount of income per year (float)
     float companyIncome = 0.0f;
-    //cost of office equipment (float)
     float equipmentCost = 200.25f;
 
-    //3 things it can do:
-    // create plugins
-    int createPlugin(); //return is related to function parameter
-
-    // set authorization methods
+    int createPlugin();
     void authEnable();
-
-    // sign legal agreements
     void signContract();
 };
-/*
 
-Thing 8) Signal Processor
-
-*/
 struct SignalProcessor
 {
-
-    //samples (float)
     float numSamples = 256.0f;
-    //channels (int)
     int numChannels = 2;
-    //type (std::string)
     std::string type = "Unknown";
-    //buffer (float)
     float numBuffer = 2.2f;
-    //program (int)
     int program = 4;
 
-    //3 things it can do:
-    // receive audio
     void receiveAudio();
-
-    // convert raw audio level to decibels
-    float convertToDecibels(); //return is related to function parameter
-
-    // send audio
+    float convertToDecibels();
     void sendAudio();
 };
-/*
 
-Thing 9) DSP Engine
-
-*/
 struct DSPEngine
 {
-
-    //value of incoming gain (double)
     double gainInputLevel = 0;
-    //duration of gain ramp (double)
     double gainRampDuration = 3482;
-    //amount of effect (float)
     float wetLevel = 33.1f;
-    //name of effect (std::string)
     std::string effect = ("Chorus" "Phase" "Flange");
-    //value of output level (float)
     float gainOutputLevel = -6.4f;
 
-    // 3 things it can do:
-    // create a chorus effect
     void createChorus();
-
-    // modify gain level
-    float modifyGain(); //return is related to function parameter
-
-    // disable output
-    bool outputEnabled(); //return is related to function parameter
+    float modifyGain();
+    bool outputEnabled();
 };
-/*
 
-Thing 10) Equalizer Audio Plugin
-
-*/
 struct EqualizerAudioPlugin
 {
-
-    //Graphical User Interface
     GraphicalUserInterface GUI;
-    //license
     License license;
-    //company
     Company company;
-    //signal processor
     SignalProcessor signalProcessor;
-    //DSP engine
     DSPEngine DSP_Engine;
 
-    // 3 things it can do:
-    // display changes to frequency level
-    float displayFreqLevelChange(); //return is displayed for user
-
-    // attenuate audio frequency level
-    float freqLevelChange(); //return is related to function parameter
-
-    // accomodate multiple hosts
+    float displayFreqLevelChange();
+    float freqLevelChange();
     void buildType();
 };
 
@@ -470,7 +355,7 @@ Be sure to make the commit message meaningful.
 Part 1e - Step 19: Request a review
 =================
 */
-} // end namespace Part1E_Instructions
+// end namespace Part1E_Instructions
 
 /*
 paste your code below
